0d1e6eb8dd7b372f0adf7fb9e65fa95bd2ab81d5
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-02 11:28:18 +0530

    learned about a few array methods

--- 50days-Js-challenge/challenge1-10.js
+++ 50days-Js-challenge/challenge1-10.js
@@ -2,17 +2,30 @@
 // Write a program to reverse a given integer number
 
 const num = 1002;
+let temp = 0; revNum = '';
 
 function reverseGivenInteger(num) {
     // write your solution here
-    let revNum = String(num).split('').reverse().join('');
+    while (num !== 0){
+        temp = num % 10;
+        revNum += temp;
+        num = Math.floor(num/10);
+    }
     return Number(revNum);
 }
-
+// output: 2001
 console.log(`Reversed integer is: ${reverseGivenInteger(num)}`)
 
 
-// Function which returns a random number in the given range
+// challenge 2
+// Write a program to reverse the words in a given string.
+
+const text = "hi, What are you learning currently";
+
+const reverseWords = (text) => {
+    
+}
+
 
 
 
---
+++ Arrays/Array.js
@@ -0,0 +1,25 @@
+// Array - an array is a collection of elements of the same datatype.
+
+/*   All array methods:
+     0️⃣ array.length - returns the lenght of the array.
+     1️⃣ array.toString() - converts the array into a string.
+     2️⃣ array.join('seperator') - converts an array to a string, can also add a seperator.
+
+     ADDING AND REMOVING ELEMENTS
+     3️⃣ array.pop() - removes and returns the last element of an array.
+     4️⃣ array.push('element') - adds a new element to the end of the array.
+     5️⃣ array.shift() - removes and returns the first element of an array.
+     6️⃣ array.unshift("element") - add a new element at the front of the array.
+     
+     MERGING AND CONCATENATION
+     1️⃣ ARRAY1.concat(ARRAY2, array3) - Combines two or more arrays into one.
+     
+     INDEX
+     1️⃣ array[index] - To access an element at a certain index.
+
+     SLICING AND SPLICING
+     1️⃣ array.splice(newItemPos, noItemToDel, "item1", "items2") - it is used to add new items to an array.
+     2️⃣ array.slice(startPos, endPos) - it removes the elements within  a given index range. If the ending range is not given, then all 
+                                         the items till the end will be removed. 
+     3️⃣ array.reverse() - it returns the array reversed.
+*/
