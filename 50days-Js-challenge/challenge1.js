3a73c50f84a8e163e29043cf6c7903c8fa250eff
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-18 13:19:42 +0530

    done with challenge 5

--- 50days-Js-challenge/challenge1.js
+++ 50days-Js-challenge/challenge1.js
@@ -17,6 +17,9 @@ function reverseGivenInteger(num) {
 console.log(`Reversed integer is: \n${reverseGivenInteger(num)}`)
 
 
+
+
+
 // challenge 2
 // Write a program to reverse the charaters in each word in a given string.
 
@@ -35,21 +38,30 @@ const reverseWords = (text) => {
 console.log(`The reversed string is \n${reverseWords(text)}`);
 
 
+
+
+
 // challenge 3
 // Write a function which can convert the time input given in 12 hours format to 24 hours format 
 const timeTo24 = (time) => {
-    if (time.includes('AM')) {
+    let arr = time.split(':');
+
+    if (time.includes('AM') || (time.startsWith("12:")) && time.includes('PM')){
         return time;
     }
+    else if(time.includes('AM') && time.startsWith("12:")){
+        let formattedTime =
+        `${12 + Number(arr[0])}:${arr[1]}:${arr[2].slice(0, 2)} AM`;
+        return formattedTime;
+        
+    }
     else {
-        let arr = time.split(':');
         let formattedTime =
             `${12 + Number(arr[0])}:${arr[1]}:${arr[2].slice(0, 2)} PM`;
         return formattedTime;
     }
 
 }
-
 // time input in 12hour format
 let cTime = new Date().toLocaleTimeString();
 console.log(`Time converted to 24 hour format: 
@@ -57,31 +69,9 @@ console.log(`Time converted to 24 hour format:
 
 
 
+
+
 // challenge 4
-//   Write a function which accepts a string argument and returns the count of characters between the first and last character 'X'
-
-
-// challenge 5
-//  Write a function to truncate a string to a certain number of words
-
-
-// challenge 6
-// Create a regular expression to validate if the given input is valid Indian mobile number or not
-
-
-// challenge 7 
-// Write a function which accepts two valid dates and returns the difference between them as number of days
-
-
-// challenge 8
-//   Write a function to check if an object is empty or not in javaScript?
-
-
-// challenge 9
-//  Write a function to remove array element based on object property?
-
-
-// challenge 10
 // Function which returns a random number in the given range
 function randomNumber(max, min) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -90,9 +80,24 @@ function randomNumber(max, min) {
 let random = randomNumber(-555, 100);
 
 // output: some random number.
-console.log(`random number between -155 and 100: \n${random}`);
+console.log(`random number between -155 and 100: \n${random}\n\n`);
 
 
+
+
+
+// challenge 5
+//   Write a function which accepts a string argument and returns the count of characters between the first and last character 'X'
+const str = 'F(X) !== G(X) !== F(X)';
+
+function getTheGapX(str) {
+    let firstIndex = str.indexOf('X') + 1;
+    let lastIndex = str.lastIndexOf('X');
+
+    return `${lastIndex - firstIndex}`;
+}
+
+console.log(`Gap between the X's: ${getTheGapX(str)}`)
 
 
 
---
+++ 50days-Js-challenge/challenge2.js
@@ -0,0 +1,18 @@
+// challenge 5
+//  Write a function to truncate a string to a certain number of words
+
+
+// challenge 6
+// Create a regular expression to validate if the given input is valid Indian mobile number or not
+
+
+// challenge 7 
+// Write a function which accepts two valid dates and returns the difference between them as number of days
+
+
+// challenge 8
+//   Write a function to check if an object is empty or not in javaScript?
+
+
+// challenge 9
+//  Write a function to remove array element based on object property?
